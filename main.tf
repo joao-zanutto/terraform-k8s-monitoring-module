fe72f97dcbe6b5315bdd6181067fc5c103061987
joao-zanutto <jpberno@gmail.com>
2024-03-14 14:24:03 -0700

    format main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "helm_release" "monitoring_stack" {
   name       = "monitoring"
-  namespace = kubernetes_namespace.monitoring.metadata[0].name
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   version    = "57"
 }
 
 resource "kubernetes_namespace" "monitoring" {
-    metadata {
-      name = var.namespace
-    }
+  metadata {
+    name = var.namespace
+  }
 }
