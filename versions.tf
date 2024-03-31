7cd299ac7e67f1a936794e59c06f854e219f0f35
joao-zanutto <jpberno@gmail.com>
2024-03-31 12:06:07 -0700

    fix: add versions file

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1.5.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.27.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+  }
+}
